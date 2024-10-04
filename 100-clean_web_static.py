5ebefcac30a76ec0e0f4ae27f312c66cbe7bd57d
ernesteNtezirizaza <erneste.ntezirizaza@gmail.com>
2024-10-04 15:39:00 +0200

    update 3-deploy_web_static.py file

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['34.226.191.215', '34.235.167.216']
 
 
 def do_clean(number=0):
